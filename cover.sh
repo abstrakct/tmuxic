6ee11b24b3468e5dd6faae9ac93afab04345468c
thunderwarrior <abstractstar@gmail.com>
2015-10-26 20:48:34 +0100

    Added support for coverart embedded in FLAC metadata tags.

--- cover.sh
+++ cover.sh
@@ -11,16 +11,27 @@ while :; do
     CURRENT_FILE=$($MPC current -f "%file%")
     CURRENT_DIR=$(dirname "$CURRENT_FILE")
 
-    FILENAME=$(find $MUSIC_DIR/"$CURRENT_DIR" -name "*[Ff]ront*[png|jpg|bmp]")
-    if [[ -z $FILENAME ]]; then
-        FILENAME=$(find $MUSIC_DIR/"$CURRENT_DIR" -name "*[Cc]over*[png|jpg|bmp]")
-        if [[ -z $FILENAME ]]; then
-            FILENAME=$(find $MUSIC_DIR/"$CURRENT_DIR" -name "*[Ff]older*[png|jpg|bmp]")
-        fi
+    # Check for coverart in FLAC metadata first
+
+    METADATA_COVER=$(metaflac --list $MUSIC_DIR/"$CURRENT_FILE" | grep "Cover (front)")
+
+    if [[ -n $METADATA_COVER ]]; then
+        FILENAME="/tmp/tmuxic-front-cover"
+        metaflac --export-picture-to=$FILENAME $MUSIC_DIR/"$CURRENT_FILE"
     fi
 
     if [[ -z $FILENAME ]]; then
-        FILENAME="/home/rolf/pictures/everything-is-shit-mindre.png"
+        FILENAME=$(find $MUSIC_DIR/"$CURRENT_DIR" -name "*[Ff]ront*[png|jpg|bmp]")
+        if [[ -z $FILENAME ]]; then
+            FILENAME=$(find $MUSIC_DIR/"$CURRENT_DIR" -name "*[Cc]over*[png|jpg|bmp]")
+            if [[ -z $FILENAME ]]; then
+                FILENAME=$(find $MUSIC_DIR/"$CURRENT_DIR" -name "*[Ff]older*[png|jpg|bmp]")
+            fi
+        fi
+
+        if [[ -z $FILENAME ]]; then
+            FILENAME="/home/rolf/pictures/everything-is-shit-mindre.png"
+        fi
     fi
 
     w3m_command="0;1;0;0;$width;$height;;;;;""$FILENAME""\n4;\n3;"
--- cover_back.sh
+++ cover_back.sh
@@ -11,7 +11,16 @@ while :; do
     CURRENT_FILE=$($MPC current -f "%file%")
     CURRENT_DIR=$(dirname "$CURRENT_FILE")
 
-    FILENAME=$(find $MUSIC_DIR/"$CURRENT_DIR" -name "*[Bb]ack*[png|jpg|bmp]")
+    METADATA_COVER=$(metaflac --list $MUSIC_DIR/"$CURRENT_FILE" | grep "Cover (back)")
+    if [[ -n $METADATA_COVER ]]; then
+        FILENAME="/tmp/tmuxic-back-cover"
+        metaflac --export-picture-to=$FILENAME $MUSIC_DIR/"$CURRENT_FILE"
+    fi
+
+    if [[ -z $FILENAME ]]; then
+        FILENAME=$(find $MUSIC_DIR/"$CURRENT_DIR" -name "*[Bb]ack*[png|jpg|bmp]")
+    fi
+
 #    if [[ -z $FILENAME ]]; then
 #        FILENAME=$(find $MUSIC_DIR/"$CURRENT_DIR" -name "*[Cc]over*[png|jpg|bmp]")
 #        if [[ -z $FILENAME ]]; then
